4d866fade41d6496daa5351b7a5191312f73c247
Vincent <cherebedov.sergey@gmail.com>
2019-10-11 00:21:03 +0300

    fixes

--- loadDictionary.js
+++ loadDictionary.js
@@ -1,11 +1,6 @@
 const request = require('request');
 const fs = require('fs');
 
-const url = process.env.DICTIONARY_URL;
+const url = process.env.DICTIONARY_URL || 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwmAFvs2FmTYZfaS6VMe3X0VbvuKCs5F94YbvcyRfD070GZ0eNvYZAZXoPuZyT4s6Wqho2wyVzeeeu/pub?gid=1987833874&single=true&output=tsv'
 
-console.log('URL', url);
-
-request(url, (err, res) => {
-  console.log('RESULT', err, res);
-  fs.writeFileSync('./dist/dictionary.csv', res.body);
-});
+request(url, (err, res) => fs.writeFileSync('./dist/dictionary.csv', res.body));
