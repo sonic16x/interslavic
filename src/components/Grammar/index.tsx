3a86cdb09e9a726f0973f438504fd8a900b5184e
Sergei Cherebedov <sergei_cherebedov@epam.com>
2019-10-15 15:24:32 +0300

    add more text

--- src/components/Grammar/index.tsx
+++ src/components/Grammar/index.tsx
@@ -16,21 +16,36 @@ class Grammar extends React.Component<IGrammarProps> {
         return (
             <div className={'container grammar' + (this.props.isVisible ? ' show' : '')}>
                 <h4>MEDŽUSLOVJANSKY JEZYK</h4>
-                <Text>{`{abeceda i pravopisanje}[B]`}</Text>
+                <Text align={'center'}>{`{abeceda i pravopisanje}[B]`}</Text>
                 <Table data={tables.tableAbeceda} fontSize={'3.5vw'} />
-                <Text>
+                <Text indent={'0.5rem'}>
                     {`V latinici i cirilici jest možno vměsto {y}[b,B] pisati prosto {i}[b,B] i vměsto {ě}[g] pisati prosto {e}[g].
                     Napr. {r{y}[b]ba→r{i}[b]ba r{ě}[g]ka→r{e}[g]ka}[B]
                     Palatizacija i eufonija: {{k}[r]→{č}[r], {h}[r]→{š}[r], {g}[r]→{ž}[r], {c}[r]{j}[p]→{č}[r], {s}[r]{j}[p]→{š}[r], {zj}[r]→{ž}[r]}[B]
                     Napr.: {Grě{k}[r]→grě{č}[r]sky pra{h}[r]→pra{š}[r]ny Bo{g}[r]→bo{ž}[r]sky pro{s}[r]{ju}[p]→pro{š}[r]{u}[p]}[B]`}
                 </Text>
+                <Text indent={'0.5rem'}>
+                    {`{1.}[B,m] Staroslovjansko jatj={ѣ}[g]={ě}[g] se može pisati bez diakritiky kako ie ili prosto e.
+                         Podobno {č}[r], {š}[r], {ž}[r] se mogut pisati kako {cz}[r], {sz}[r], {zs}[r].`}
+                </Text>
+                <Text indent={'0.5rem'}>
+                    {`{2.}[B,m] Staroslovjansko щ jest na počatkah slov kako {šč}[r], ale potom kako {č}[r].(napr. {ščit}[r], {pomoč}[r], {občina}[r], {svěča}[r])`}
+                </Text>
+                <Text indent={'0.5rem'}>
+                    {`{3.}[B,m] V cirilici možno koristiti ligatury
+                        {шч}[r] → {щ}[r] {ьу}[r] / {ју}[r] → {ю}[r] {ьа}[r] / {ја}[r] → {я}[r]
+                        Medžuslovjansky jezyk drži morfologično pravopisanje. Koren slov se piše
+                        rovno v vsih padežah. Englijske, latinske i grečske slova imajut svoje orig.
+                        pravopisanje ale s medžuslovjanskymi zakončenjami ({architektura}[r], {biologija}[r] ...).`}
+                </Text>
+                <Text align={'center'}>{'{imeniky}[B]'}</Text>
                 <Table data={tables.tableBrat} />
                 <Table data={tables.tableMuz} />
                 <Table data={tables.tableZena} />
                 <Table data={tables.tableKost} />
                 <Table data={tables.tableSelo} />
                 <Table data={tables.tableDen} />
-                <Text>{`{naučno pravopisanje v slovniku i cirilica}[B]`}</Text>
+                <Text align={'center'}>{`{naučno pravopisanje v slovniku i cirilica}[B]`}</Text>
                 <div className={'tablesRow'}>
                     <Table fontSize={'2.7vw'} data={tables.tableNauc1} />
                     <Table fontSize={'2.7vw'} data={tables.tableNauc2} />
