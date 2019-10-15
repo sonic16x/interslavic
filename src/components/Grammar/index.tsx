465a5819cc09b1b55d05fd2ba0c97e06ebb1a8b2
Sergei Cherebedov <sergei_cherebedov@epam.com>
2019-10-15 16:37:59 +0300

    More fixes

--- src/components/Grammar/index.tsx
+++ src/components/Grammar/index.tsx
@@ -17,8 +17,8 @@ class Grammar extends React.Component<IGrammarProps> {
             <div className={'container grammar' + (this.props.isVisible ? ' show' : '')}>
                 <h4>MEDŽUSLOVJANSKY JEZYK</h4>
                 <Text align={'center'}>{`{abeceda i pravopisanje}[B]`}</Text>
-                <Table data={tables.tableAbeceda} fontSize={'3.5vw'} />
-                <Text indent={'0.5rem'}>
+                <Table fontSize={'3.5vw'} data={tables.tableAbeceda} />
+                <Text>
                     {`V latinici i cirilici jest možno vměsto {y}[b,B] pisati prosto {i}[b,B] i vměsto {ě}[g] pisati prosto {e}[g].
                     Napr. {r{y}[b]ba→r{i}[b]ba r{ě}[g]ka→r{e}[g]ka}[B]
                     Palatizacija i eufonija: {{k}[r]→{č}[r], {h}[r]→{š}[r], {g}[r]→{ž}[r], {c}[r]{j}[p]→{č}[r], {s}[r]{j}[p]→{š}[r], {zj}[r]→{ž}[r]}[B]
@@ -39,10 +39,26 @@ class Grammar extends React.Component<IGrammarProps> {
                         pravopisanje ale s medžuslovjanskymi zakončenjami ({architektura}[r], {biologija}[r] ...).`}
                 </Text>
                 <Text align={'center'}>{'{imeniky}[B]'}</Text>
-                <Table data={tables.tableBrat} />
-                <Table data={tables.tableMuz} />
-                <Table data={tables.tableZena} />
-                <Table data={tables.tableKost} />
+                <Table fontSize={'2.7vw'} data={tables.tableBrat} />
+                <Table fontSize={'2.7vw'} data={tables.tableMuz} />
+                <Text>
+                    {`Mužske objekty, ktore aktivno dělajut někaky proces, sut {životne}[i]
+                        (od pytanja KTO?) i po tutoj pričině imajut akuzativ rovny s genitivom.
+                        Ostalne objekty sut {neživotne}[i] (od pytanija ČTO) i imajut v jednině akuzativ
+                        rovny s nominativom.
+                      napr.: {gospod peče {hlěb}[r].}[B] ({hlěb}[r,B] ne može pečti = jest neživotny)
+                      {gospod vidi {člověka}[r]}[B]. ({člověk}[r,B] može viděti = jest životny)
+                    `}
+                </Text>
+                <Text>
+                    {`Slova mužskogo roda zakončeni na -{a}[r] imajut v jednině klonjenje po tvrdom
+                            vzoru {žena}[r] ili mekkom vzoru {duša}[r], ale v množině i dvojině imajut normalny
+                            mužsky vzor.
+                            napr.: {vladyk-{a}[r], vladyk-{y}[b], vladyk-{ě}[g], vladyk-{u}[p], vladyk-{o}[p],}[B] ... (jednina)
+                            {vladyk-{i}[b], vladyk-{ov}[p], vladyk-{a}[r]m, vladyk-{a}[r]m{i}[b],}[B] ... (množina)`}
+                </Text>
+                <Table fontSize={'2.7vw'} data={tables.tableZena} />
+                <Table fontSize={'2.7vw'} data={tables.tableKost} />
                 <Table data={tables.tableSelo} />
                 <Table data={tables.tableDen} />
                 <Text align={'center'}>{`{naučno pravopisanje v slovniku i cirilica}[B]`}</Text>
