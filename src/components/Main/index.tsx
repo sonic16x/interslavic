d8cf70e7c929a9407c556a8ce21e890ae12e2468
Vincent <cherebedov.sergey@gmail.com>
2019-10-17 21:57:10 +0300

    Start with history and routing

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -6,11 +6,14 @@ import { Loader } from 'components/Loader';
 import Dictionary from 'components/Dictionary';
 import Grammar from 'components/Grammar';
 import './index.scss';
-import { fetchDictionary } from 'actions';
+import { fetchDictionary, setPageAction } from 'actions';
+import { getPage } from 'reducers';
 
 interface IMainProps {
     isLoading: boolean;
+    page: string;
     loadDictionary: () => void;
+    setPage: (page: string) => void;
 }
 
 const dictionaryUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwmAFvs2FmTYZfaS6VMe3X0VbvuKCs5F94YbvcyRfD070GZ0eNvYZAZXoPuZyT4s6Wqho2wyVzeeeu/pub?gid=1987833874&single=true&output=tsv';
@@ -19,6 +22,8 @@ class Main extends React.Component<IMainProps> {
     constructor(props) {
         super(props);
         this.props.loadDictionary();
+        this.onChangeUrl = this.onChangeUrl.bind(this);
+        window.onpopstate = this.onChangeUrl;
     }
     public render() {
         return (
@@ -31,15 +36,22 @@ class Main extends React.Component<IMainProps> {
             </>
         );
     }
+    private onChangeUrl() {
+        const page = getPage();
+        if (this.props.page !== page) {
+            this.props.setPage(page);
+        }
+    }
 }
 
-function mapStateToProps({ isLoading }) {
-    return { isLoading };
+function mapStateToProps({ isLoading, page }) {
+    return { isLoading, page };
 }
 
 function mapDispatchToProps(dispatch) {
     return {
         loadDictionary: () => fetchDictionary(dictionaryUrl)(dispatch),
+        setPage: (page) => dispatch(setPageAction(page)),
     };
 }
 
