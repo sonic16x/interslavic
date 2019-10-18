bb9cf3a6349fa59be28bb824e394f3da75dd549b
Sergei Cherebedov <sergei_cherebedov@epam.com>
2019-10-18 13:01:52 +0300

    add gdpr alert

---
+++ src/components/GDPR/index.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import './index.scss';
+
+interface IGDPRState {
+    show: boolean;
+}
+
+export default class extends React.Component<{}, IGDPRState> {
+    constructor(props) {
+        super(props);
+        const showAlert = localStorage.getItem('gdprAlert') !== 'false';
+        this.state = {
+            show: showAlert,
+        };
+    }
+    public render() {
+        const link = 'https://yandex.com/legal/metrica_agreement/';
+        return (
+            <div
+                className={'alert alert-warning gdprAlert' + (!this.state.show ? ' hide' : '')}
+                role={'alert'}
+            >
+                <strong>This page uses cookies for analytics: </strong><a href={link} target={'_blank'}>Readm more</a>
+                <button
+                    type={'button'}
+                    className={'btn btn-primary'}
+                    onClick={() => this.hideAlert()}
+                >
+                    Alright
+                </button>
+            </div>
+        );
+    }
+    private hideAlert() {
+        this.setState({show: false});
+        localStorage.setItem('gdprAlert', 'false');
+    }
+}
