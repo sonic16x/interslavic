5142798f60a2e18622ba4f7492081bec6d0cabbd
Vincent <cherebedov.sergey@gmail.com>
2020-05-02 21:10:59 +0300

    Change google sheet table

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-export const dictionaryUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwmAFvs2FmTYZfaS6VMe3X0VbvuKCs5F94YbvcyRfD070GZ0eNvYZAZXoPuZyT4s6Wqho2wyVzeeeu/pub?gid=1987833874&single=true&output=tsv';
-export const worksheetUrl = 'https://docs.google.com/spreadsheets/d/14b7B41A847_PDD6S3dFBOVBIkt_Cdxnt1m4e6NjrWP0/edit?usp=sharing';
+export const dictionaryUrl = 'https://docs.google.com/spreadsheets/u/0/d/1N79e_yVHDo-d026HljueuKJlAAdeELAiPzdFzdBuKbY/export?format=tsv';
+export const worksheetUrl = 'https://docs.google.com/spreadsheets/d/1N79e_yVHDo-d026HljueuKJlAAdeELAiPzdFzdBuKbY';
 export const alphabetTypes = [
     {
         name: 'latin',
