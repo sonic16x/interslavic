74cd69103092cee77b34f59c865db750257c127e
Yaroslav Serhieiev <yaroslavs@wix.com>
2020-12-30 11:41:02 +0200

    wip: add animation and translations

--- src/components/SurveyBanner/index.tsx
+++ src/components/SurveyBanner/index.tsx
@@ -1,38 +1,51 @@
 import * as React from 'react';
+import useDimensions from 'react-cool-dimensions';
 import classNames from 'classnames';
 import { t } from 'translations';
 import './index.scss';
 import CloseImage from './images/close.svg';
+import { useLang } from 'hooks/useLang';
 
-export const SurveyBanner: React.FC =
-  () => {
-    return (
-      <aside className='survey-banner'>
-        <h1 className='survey-banner__caption'>
-          <span lang='sla-Latn-x-isv'>Medžuslovjansky Spis</span>
-          <span hidden={true} lang='sla-Cyrl-x-isv'>Меджусловјанскы Спис</span>
-        </h1>
-        <div className='survey-banner__bottom'>
-          <p className='survey-banner__description'>
-            <span lang='sla-Latn-x-isv'>S pomoćjų tutogo spisa my hćemo kalkulovati kako mnogo jest ljudij govoręćih MS językom ili aktualno učęćih sę go.</span>
-            <span hidden={true} lang='sla-Cyrl-x-isv'>С помочју тутого списа мы хчемо калкуловати како много јест људиј говоречих МС језыком или актуално учечих се го.</span>
-          </p>
-          <div className='survey-banner__action'>
-            <a
-               className='survey-banner__button'
-               role='button'
-               target='_blank'
-               rel='noopener noreferrer'
-               href='https://docs.google.com/forms/d/e/1FAIpQLSeMWWhHKKd7pQolv-EYPAxKjL5ppMb1ABs751AVz-TAfK1nnA/viewform?usp=sf_link'
-            >
-              <span lang='sla-Latn-x-isv'>Učęstvovati</span>
-              <span hidden={true} lang='sla-Cyrl-x-isv'>Учествовати</span>
-            </a>
-          </div>
+function useBannerHeight() {
+  const { ref, height } = useDimensions({});
+
+  React.useEffect(() => {
+    const app = document.getElementById('app');
+    app.style.setProperty('--survey-banner-height', `${height}`);
+  }, [height]);
+
+  return ref;
+}
+
+export const SurveyBanner: React.FC = () => {
+  const lang = useLang() as any;
+  const [isClosing, setClosing] = React.useState(false);
+  const close = React.useCallback(() => setClosing(true), [setClosing]);
+
+  return (
+    <aside ref={useBannerHeight()} className={classNames('survey-banner', isClosing && 'closing')}>
+      <h1 className='survey-banner__caption'>
+        {t('surveyBanner.caption', { lang })}
+      </h1>
+      <div className='survey-banner__bottom'>
+        <p className='survey-banner__description'>
+          {t('surveyBanner.description', { lang })}
+        </p>
+        <div className='survey-banner__action'>
+          <a
+             className='survey-banner__button'
+             role='button'
+             target='_blank'
+             rel='noopener noreferrer'
+             href='https://docs.google.com/forms/d/e/1FAIpQLSeMWWhHKKd7pQolv-EYPAxKjL5ppMb1ABs751AVz-TAfK1nnA/viewform?usp=sf_link'
+          >
+            {t('surveyBanner.action')}
+          </a>
         </div>
-        <button title={t('close')} aria-label={t('close')} className='survey-banner__close'>
-          <CloseImage />
-        </button>
-      </aside>
-    );
-  };
+      </div>
+      <button title={t('close')} aria-label={t('close')} className='survey-banner__close' onClick={close}>
+        <CloseImage />
+      </button>
+    </aside>
+  );
+};
