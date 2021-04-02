0fac93686855798b2098eb5928ab2d9301bd9d09
Yaroslav Serhieiev <noomorph@gmail.com>
2021-04-02 10:10:22 +0300

    Revert "feat: add 1st April logo"
    This reverts commit bb804f990835bed61eabd02106c7e965954e1cf8.

--- src/utils/isApril1.ts
+++
@@ -1,8 +0,0 @@
-export function isApril1() {
-  const seed = typeof localStorage !== 'undefined'
-    ? localStorage.getItem('APRIL_1_TEST')
-    : null;
-
-  const now = new Date(seed || undefined);
-  return now.getMonth() === 3 && now.getDate() === 1 && now.getFullYear() === 2021;
-}
