d91cd1d44436075fda09665a79ce16e5ba1b6304
Yaroslav Serhieiev <noomorph@gmail.com>
2021-08-27 10:48:21 +0300

    revert: consts change until Sergey approves

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,3 @@
-export const dictionaryUrl = 'https://docs.google.com/spreadsheets/u/0/d/1N79e_yVHDo-d026HljueuKJlAAdeELAiPzdFzdBuKbY/export?format=tsv';
-export const dictionaryUrlAdd = 'https://docs.google.com/spreadsheets/u/2/d/1N79e_yVHDo-d026HljueuKJlAAdeELAiPzdFzdBuKbY/export?format=tsv&id=1N79e_yVHDo-d026HljueuKJlAAdeELAiPzdFzdBuKbY&gid=2005431854';
-export const worksheetUrl = 'https://docs.google.com/spreadsheets/d/1N79e_yVHDo-d026HljueuKJlAAdeELAiPzdFzdBuKbY';
-
 export const alphabetTypes = [
     {
         name: 'latin',
