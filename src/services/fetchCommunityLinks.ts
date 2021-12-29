a39bc295db2e8c047195146b5f79eca21de48980
Vincent <cherebedov.sergey@gmail.com>
2021-12-29 03:29:24 +0300

    online fix

--- src/services/fetchCommunityLinks.ts
+++ src/services/fetchCommunityLinks.ts
@@ -4,11 +4,16 @@ import { setBadges, setCommunityLinks } from 'actions';
 
 import { getObjFromTable } from 'utils/getObjFromTable';
 import { getTableDataUrl } from 'utils/getTableDataUrl';
+import { isOnline } from 'utils/isOnline';
 import { parseTsvTable } from 'utils/parseTsvTable';
 
 const communityFeedLink = getTableDataUrl(communityLinksTable.spreadsheetId, communityLinksTable.sheetId);
 
 export async function fetchCommunityLinks(dispatch, currentCommunityLinks) {
+    if (!isOnline()) {
+        return;
+    }
+
     const communityLinks = await fetch(communityFeedLink)
         .then((res) => res.text())
         .then((text) => {
