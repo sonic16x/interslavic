d657569741cb01c19afb0bb89ef07097e981f092
Vincent <cherebedov.sergey@gmail.com>
2022-01-07 00:00:47 +0300

    Remove error

--- src/middlewares/urlParamsMiddleware.ts
+++ src/middlewares/urlParamsMiddleware.ts
@@ -8,14 +8,18 @@ export function urlParamsMiddleware({ getState }) {
             return result;
         }
 
-        const { fromText, lang } = getState();
-        const query = toQueryString({
+        const { fromText, lang, page } = getState();
+        let query = page === 'dictionary' ? toQueryString({
             text: fromText,
             lang: `${lang.from}-${lang.to}`,
-        })
+        }) : '';
+
+        if (query) {
+            query = `?${query}`;
+        }
 
         const { protocol, host, pathname } = window.location;
-        const path = `${protocol}//${host}${pathname}?${query}`;
+        const path = `${protocol}//${host}${pathname}${query}`;
         window.history.pushState({ path },'', path);
 
         return result;
