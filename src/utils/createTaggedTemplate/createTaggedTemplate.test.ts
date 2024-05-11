841ef583536eaa3402ff76315aaef3bc46ae77e8
Yaroslav Serhieiev <noomorph@gmail.com>
2024-05-11 09:40:55 +0300

    refactor: smart transliteration in translate

---
+++ src/utils/createTaggedTemplate/createTaggedTemplate.test.ts
@@ -0,0 +1,37 @@
+import { getCyrillic } from '../getCyrillic';
+
+import { createTaggedTemplate } from './createTaggedTemplate';
+
+describe('createTaggedTemplate', () => {
+    const toCyrillic = (s: unknown) => getCyrillic(String(s), '3');
+
+    test('applies transformation to template strings', () => {
+        const Cyrl = createTaggedTemplate(toCyrillic, 'strings');
+        const result = Cyrl`Pozdrav, ${'world'}! Kako ${'you'} jesi?`;
+        expect(result).toBe('Поздрав, world! Како you јеси?');
+    });
+
+    test('applies transformation to interpolated values', () => {
+        const Cyrl = createTaggedTemplate(toCyrillic, 'values');
+        const result = Cyrl`Hello, ${'Světe'}! How are ${'ty'}?`;
+        expect(result).toBe('Hello, Свєте! How are ты?');
+    });
+
+    test('handles empty template strings', () => {
+        const Cyrl = createTaggedTemplate(toCyrillic, 'strings');
+        const result = Cyrl``;
+        expect(result).toBe('');
+    });
+
+    test('handles template strings without interpolated values', () => {
+        const Cyrl = createTaggedTemplate(toCyrillic, 'strings');
+        const result = Cyrl`Pozdrav, světe!`;
+        expect(result).toBe('Поздрав, свєте!');
+    });
+
+    test('handles interpolated values of different types', () => {
+        const Cyrl = createTaggedTemplate(toCyrillic, 'values');
+        const result = Cyrl`Value 1: ${123}, Value 2: ${true}, Value 3: ${null}, Value 4: ${undefined}`;
+        expect(result).toBe('Value 1: 123, Value 2: труе, Value 3: нулл, Value 4: ундефинед');
+    });
+});
---
+++ src/utils/parseI18nString/parseI18nString.test.ts
@@ -0,0 +1,31 @@
+import { parseI18nString } from './parseI18nString';
+
+describe('parseI18nString', () => {
+    it('should return empty string when given empty string', () => {
+        assertEquals(parseI18nString(''), resultOf``);
+    });
+
+    it('should return string when given string without substitutions', () => {
+        assertEquals(parseI18nString('string'), resultOf`string`);
+    });
+
+    it('should return string and one substitution when given string with one substitution', () => {
+        assertEquals(parseI18nString('string {substitution}'), resultOf`string ${`substitution`}`);
+    });
+
+    it('should return string and two substitutions when given string with two substitutions', () => {
+        assertEquals(parseI18nString('{substitution} string {substitution2}'), resultOf`${`substitution`} string ${`substitution2`}`);
+    });
+});
+
+function resultOf(strings: TemplateStringsArray, ...substitutions: unknown[]): [TemplateStringsArray, ...unknown[]] {
+    return [strings, ...substitutions];
+}
+
+function assertEquals(actual: [TemplateStringsArray, ...unknown[]], expected: [TemplateStringsArray, ...unknown[]]) {
+    // We cannot use `toEqual` because `raw` property does not play well with array comparison
+    expect(actual[0].raw).toEqual(expected[0].raw);
+    expect([...actual[0]]).toEqual([...expected[0]]);
+
+    expect(actual.slice(1)).toEqual(expected.slice(1));
+}
