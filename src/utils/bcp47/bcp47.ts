6d50adba90f9a58cd5abc219ca7e916eea1d3f59
Yaroslav Serhieiev <noomorph@gmail.com>
2024-05-12 19:41:55 +0300

    fix: use strict transliteration

--- src/utils/bcp47/bcp47.ts
+++ src/utils/bcp47/bcp47.ts
@@ -1,8 +1,4 @@
 export function toBCP47(code: string) {
-    if (code.startsWith('isv')) {
-        return code.replace('isv', 'art-x-isv');
-    }
-
     if (code === 'sr') {
         return 'sr-Cyrl';
     }
--- src/utils/getCyrillic/getCyrillic.ts
+++ src/utils/getCyrillic/getCyrillic.ts
@@ -1,16 +1,17 @@
 import { transliterate } from '@interslavic/utils';
 
-export function getCyrillic(text: string, flavorisationType: string): string {
+export function getCyrillic(text: string, flavorisationType: string, loose = false): string {
     if (!text) {
         return '';
     }
 
+    const strict = !loose;
     switch (flavorisationType) {
-        case '1': return transliterate(text, 'art-Cyrl-x-interslv-iotated-ext');
-        case '2': return transliterate(text, 'art-Cyrl-x-interslv-etym');
-        case '4': return transliterate(text, 'art-Cyrl-x-interslv-sloviant');
-        case 'J': return transliterate(text, 'art-Cyrl-x-interslv-southern');
-        case 'S': return transliterate(text, 'art-Cyrl-x-interslv-northern');
-        default: return transliterate(text, 'art-Cyrl-x-interslv');
+        case '1': return transliterate(text, 'isv-Cyrl-x-iotated-ext', strict);
+        case '2': return transliterate(text, 'isv-Cyrl-x-etymolog', strict);
+        case '4': return transliterate(text, 'isv-Cyrl-x-sloviant', strict);
+        case 'J': return transliterate(text, 'isv-Cyrl-x-southern', strict);
+        case 'S': return transliterate(text, 'isv-Cyrl-x-northern', strict);
+        default: return transliterate(text, 'isv-Cyrl', strict);
     }
 }
