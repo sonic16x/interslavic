8e337935a19e2ed2a651cada5bae3507bdd69c06
sonic16x <cherebedov.sergey@gmail.com>
2024-11-19 19:59:26 +0700

    Fix demo

---
+++ src/server/responseError.ts
@@ -0,0 +1,8 @@
+import { responseHeaders } from "./headers";
+
+export function responseError(error) {
+    return new Response(JSON.stringify({ error }), {
+        status: 500,
+        headers: responseHeaders,
+    });
+}
