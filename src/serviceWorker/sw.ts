654aa129d6699b512d519d4ed80bd56247ab8581
Sergey Cherebedov <cherebedov.sergey@gmail.com>
2024-11-24 04:17:44 +0700

    Fix sw cache (#345)
    * Fix sw cache
    
    * Fix sw cache
    
    * Fix sw cache

--- src/serviceWorker/sw.ts
+++ src/serviceWorker/sw.ts
@@ -1,9 +1,10 @@
 import { CASH_URLS } from './cashUrls';
 
+// Add to cache all data
 self.addEventListener("install", (event: any) => {
     event.waitUntil(
         caches
-            .open(`${VERSION}fundamentals`)
+            .open(VERSION)
             .then((cache) => cache.addAll(CASH_URLS))
     );
 });
@@ -13,33 +14,45 @@ self.addEventListener("fetch", (event: any) => {
         return;
     }
 
+    // Try to find data in cache
     event.respondWith(
         caches
             .match(event.request)
-            .then((cached) => {
-                const networked = fetch(event.request)
-                    .then((response: any) => {
-                        const cacheCopy = response.clone();
-
+            .then((cache) => {
+                // Request data from network
+                const network = fetch(event.request)
+                    .then((response) => (
                         caches
-                            .open(`${VERSION}pages`)
-                            .then((cache) => cache.put(event.request, cacheCopy))
+                            .open(VERSION)
+                            .then((cache) => cache.put(event.request, response.clone())) // Update cache
+                            .then(() => response)
+                    ));
 
-                        return response;
-                    });
-
-                return cached || networked;
+                // Prefer get data from cache here
+                return cache || network;
             })
+    );
+
+    // Update cache in background
+    event.waitUntil(
+        caches
+            .open(VERSION)
+            .then((cache) => (
+                fetch(event.request).then((response) =>
+                    cache.put(event.request, response.clone())
+                )
+            ))
     );
 });
 
 self.addEventListener("activate", (event: any) => {
+    // Delete old cache versions
     event.waitUntil(
         caches
             .keys()
             .then((keys) => Promise.all(
                 keys
-                    .filter((key) => !key.startsWith(VERSION))
+                    .filter((key) => key !== VERSION)
                     .map((key) => caches.delete(key))
             ))
     );
