cf7cebd04995fef1086ac187cea468de93863f94
Sergey Cherebedov <cherebedov.sergey@gmail.com>
2024-11-24 02:06:19 +0700

    Fix enable pages (#343)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -64,7 +64,7 @@ export const Header =
 
                     return (defaultPages.includes(value) || enabledPages.includes(value))
                 })
-        ), [IS_COM, pages])
+        ), [IS_COM, pages, enabledPages])
 
         const showBadges = useMemo(() => (
             filteredPages.some(({ value }) => (badges.includes(value)))
