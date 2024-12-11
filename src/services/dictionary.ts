87d38e1ffef85875a56612a60c0f4d5ea4bade48
Sergey Cherebedov <cherebedov.sergey@gmail.com>
2024-12-12 01:05:49 +0700

    Fix search index sort (#363)
    * Fix search index sort
    
    * Fix search index sort #1

--- src/services/dictionary.ts
+++ src/services/dictionary.ts
@@ -323,8 +323,8 @@ class DictionaryClass {
         ].forEach((lang) => {
             searchIndex[lang] = Array.from(this.splittedMap[lang].keys()).map((key: string) => [
                 key,
-                this.splittedMap[lang].get(key),
-            ])
+                this.splittedMap[lang].get(key).sort(),
+            ]).sort((a, b) => a[1][0].localeCompare(b[1][0]))
         })
 
         return searchIndex
