76e3330019092d62f825029d8462cfee57579086
sonic16x <cherebedov.sergey@gmail.com>
2025-03-25 11:14:51 +0700

    Refactoring

--- src/components/ResultsCard/ResultsCard.tsx
+++ src/components/ResultsCard/ResultsCard.tsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux'
 import { t } from 'translations'
 
 import { showModalDialog } from 'actions'
-import { IAlphabets, MODAL_DIALOG_TYPES } from 'reducers'
+import { MODAL_DIALOG_TYPES } from 'reducers'
 
 import { ITranslateResult } from 'services'
 
@@ -18,7 +18,7 @@ import {
     translateAbbr,
 } from 'utils'
 
-import { Clipboard, Hint, ResultsCardActions } from 'components'
+import { Clipboard, Hint, ResultsCardActions, ResultsCardOriginal } from 'components'
 
 import './ResultsCard.scss'
 
@@ -26,73 +26,6 @@ interface IResultsCardProps {
     item: ITranslateResult;
     short: boolean;
     index: number;
-}
-
-interface IResultsCardOriginalProps {
-    item: ITranslateResult;
-    alphabets: IAlphabets;
-    caseQuestions: boolean;
-    short: boolean;
-}
-
-const ResultsCardOriginal = ({ item, alphabets, caseQuestions, short }: IResultsCardOriginalProps) => {
-    let latin = item.original
-    if (item.add) {
-        latin += ` ${item.add}`
-    }
-
-    let cyrillic = item.originalCyr
-    if (item.addCyr) {
-        cyrillic += ` ${item.addCyr}`
-    }
-
-    let gla = item.originalGla
-    if (item.addGla) {
-        gla += ` ${item.addGla}`
-    }
-
-    const result = []
-
-    if (alphabets.latin) {
-        result.push({
-            str: latin,
-            caseInfo: caseQuestions && item.caseInfo,
-            lang: 'isv-Latin',
-        })
-    }
-
-    if (alphabets.cyrillic) {
-        result.push({
-            str: cyrillic,
-            caseInfo: item.caseInfoCyr,
-            lang: 'isv-Cyrl',
-        })
-    }
-
-    if (alphabets.glagolitic) {
-        result.push({
-            str: gla,
-            caseInfo: item.caseInfoGla,
-            lang: 'isv-Glag',
-        })
-    }
-
-    return (
-        <>
-            <span className="words">
-                {result.map(({ str, caseInfo }) => (
-                    <span className="word" key={str}>
-                        <Clipboard str={str}/>
-                        {caseInfo && <span className="caseInfo">({caseInfo})</span>}
-                    </span>
-                ))}
-            </span>
-            {!caseQuestions && item.caseInfo &&
-                <span className="caseInfo">(+{t(`case${item.caseInfo.slice(1)}`)})</span>
-            }
-            {!short && item.ipa && <span className="ipa">[{item.ipa}]</span>}
-        </>
-    )
 }
 
 const WordStatus = ({ item, onClick }: { item: ITranslateResult, onClick: () => void }) => {
