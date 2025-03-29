34c4b84e1ccba03a2218bd6650c4976e6109d4e4
sonic16x <cherebedov.sergey@gmail.com>
2025-03-30 02:24:02 +0700

    New icon

--- scripts/build-cache-list.ts
+++ scripts/build-cache-list.ts
@@ -1,12 +1,14 @@
 import * as fs from 'fs'
 
 const assets = fs.readdirSync('./dist/assets')
+const icons = fs.readdirSync('./dist/icons')
 const data = fs.readdirSync('./dist/data')
 
 const cacheList = [
     '/',
     'sw.js',
     ...assets.map((item) => `/assets/${item}`),
+    ...icons.map((item) => `/icons/${item}`),
     ...data.map((item) => `/data/${item}`),
 ]
 
